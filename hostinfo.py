cc5917bd9ad2ded723f7950c4f17dd01d7dd3d1f
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-05-04 19:11:32 -0300

    Create hostinfo.py

---
+++ hostinfo.py
@@ -0,0 +1,20 @@
+import requests
+import socket
+print("PYTHON SHELL v1.0\n")
+
+urll = input( 'DIGITE O ENDEREÇO DO HOST: ' )
+#A biblioteca sockert processa o https sozinha, a variável
+#c1 é para usar o mesmo input no comabdo do requests
+#assim o usuário não precisa digitar novamente
+c1 = 'http://'
+
+url = c1+urll
+urx = urll
+x = requests.get(url)
+y = socket.gethostbyname(urx)
+
+
+print('Host de destino.....',urll)
+print('Resposta............',x.status_code)
+print('IP do host..........',y)
+
