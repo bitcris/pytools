73de3603960f886e50bdb5edec734d5ed345e0a4
Cristiano Ferreira <96497604+crisRPG@users.noreply.github.com>
2022-05-14 01:32:23 -0300

    Update ipinfo.py

--- ipinfo.py
+++ ipinfo.py
@@ -1,22 +1,28 @@
 
 #Verificar o IP público e local
-
-
-from requests import get
+from datetime import datetime
+from requests import get                                  
 import socket
 
+i = datetime.now()
+ini = int(i.strftime("%S"))
 
-print("IPINFO V:1.0 - Desenvolvido por Cristiano Ferreira")
+print("IPINFO V:1.0 - Desenvolvido por Cristiano Ferreira")               
 
+#varificando o IP público                                 
+ip = get('https://api.ipify.org').text                    
+print("IP PÚBLICO:",ip)     
 
-
-#varificando o IP público
-ip = get('https://api.ipify.org').text
-print("IP PÚBLICO:",ip)
-
-
-#verificando o IP local
-host = socket.gethostname()
+                                                                                                                        #verificando o IP local                                   
+host = socket.gethostname()                               
 lip = socket.gethostbyname(host)
-
 print("IP LOCAL:", lip)
+
+
+f = datetime.now()
+fim = int(f.strftime("%S"))
+
+tmp = fim - ini
+
+print("TEMPO",tmp,"s")
+
